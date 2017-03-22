881711f314e54447af09ad9d741358af6874509a
Scott Brenden <scott.b.brenden@gmail.com>
2017-03-22 16:41:48 -0700

    done and streched

--- starter-code/server.js
+++ starter-code/server.js
@@ -10,17 +10,20 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// DONE: Include all of the static resources as an argument to app.use()
 app.use(express.static('./public'));
 
 app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
+  // DONE: Using the response object, send the index.html file back to the user
   console.log('Scott What is up you?');
   response.sendFile('public/index.html', {root: '.'});
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+app.use(function(request, response){
+  response.sendFlile('public/new.html', {root: '.'});
+});
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`your app in on localhost:${PORT}`);
 });
